03c836e73f1d2a979b3f6dd15bc79c4780c13042
Fernando Amaya <fernando.r.amaya@hotmail.com>
2022-07-14 20:56:58 -0300

    ya no se cuanto es

--- server.js
+++ server.js
@@ -20,13 +20,23 @@ const path = require("path")
 
 const port = process.env.PORT || 8000
 
-if(process.env.NODE_ENV === "production") {
+/* if(process.env.NODE_ENV === "production") {
     app.use(express.static('build'));
-    /* app.get('*', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    }) */
-} 
+    })
+} */ 
+
+if(process.env.NODE_ENV === 'production'){
+
+    app.use(express.static(path.join(dirname,'client/build')))
+     app.get('*',(req,res)=>{
+         res.sendFile(path.resolve(dirname,'client/build/index.html'))
+     })
+}
+
 app.listen(port, (err) => {
     if(err) return console.log(err);
     console.log('server running on port: ', port);
 })
+

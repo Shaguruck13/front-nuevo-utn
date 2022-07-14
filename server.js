6c350e304a919259124bb3648cbc2122427cc715
Fernando Amaya <fernando.r.amaya@hotmail.com>
2022-07-14 18:13:40 -0300

    sin import

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.use ("/", (req,res) => {
 const port = process.env.PORT || 8000
 app.listen(port) */
 
-import express from "express"
+const express = require("express")
 const app = express();
 
-import path from "path";
+const path = require("path")
 
 const port = process.env.PORT || 8000
 

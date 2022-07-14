4ca94cb65caa5ec24c509a6b8a149100c2ded8df
Fernando Amaya <fernando.r.amaya@hotmail.com>
2022-07-14 17:52:37 -0300

    server nuevo

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express";
+/* import express from "express";
 import serveStatic from "serve-static";
 import path from "path";
 
@@ -11,4 +11,22 @@ app.use ("/", (req,res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"))
 })
 const port = process.env.PORT || 8000
-app.listen(port)
+app.listen(port) */
+
+import express from "express"
+const app = express();
+
+import path from "path";
+
+const port = process.env.PORT || 8000
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static('build'));
+    app.get('*', (req, res) => {
+        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    })
+} 
+app.listen(port, (err) => {
+    if(err) return console.log(err);
+    console.log('server running on port: ', port);
+})

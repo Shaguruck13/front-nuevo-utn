7fc3e5184a761f0a5fc85c0cfe0d8812f119e50e
Fernando Amaya <fernando.r.amaya@hotmail.com>
2022-07-14 19:56:43 -0300

    cuarta

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 8000
 if(process.env.NODE_ENV === "production") {
     app.use(express.static('build'));
     app.get('*', (req, res) => {
-        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     })
 } 
 app.listen(port, (err) => {

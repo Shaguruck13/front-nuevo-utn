e9372093baae1efcf3fc3be4bb62e46acf820065
Fernando Amaya <fernando.r.amaya@hotmail.com>
2022-07-14 21:03:13 -0300

    novena

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ const port = process.env.PORT || 8000
 
 if(process.env.NODE_ENV === "production") {
     app.use(express.static('build'));
-    app.get('*', (req, res) => {
+ /*    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    })
+    }) */
 } 
 
 
